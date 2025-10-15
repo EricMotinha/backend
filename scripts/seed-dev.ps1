ef02c8b7cfd93cc1b86f13c20afa7c0ca81fd39d
EricMota <ericmotinha@yahoo.com.br>
2025-10-15 07:50:17 -0300

    fix: seed sem BOM e script seed-dev

---
+++ scripts/seed-dev.ps1
@@ -0,0 +1,2 @@
+﻿$env:DATABASE_URL = 'postgres://postgres:Bonja123@localhost:5432/Casamenteiro?sslmode=disable'
+& 'C:\Program Files\PostgreSQL\18\bin\psql.exe' $env:DATABASE_URL -v ON_ERROR_STOP=1 -f .\db\seeds\dev_seed.sql
