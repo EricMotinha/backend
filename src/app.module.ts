e9562a4549388601bef9fff9b202d0ececff30e1
EricMota <ericmotinha@yahoo.com.br>
2025-10-17 08:34:49 -0300

    chore: AppModule sem DbModule (evita DB no boot)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,23 @@
-﻿// src/app.module.ts
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { DiscoveryModule } from './discovery/discovery.module';
-import { SwipesModule } from './swipes/swipes.module';
-import { MatchesModule } from './matches/matches.module';
-import { ConversationsModule } from './conversations/conversations.module';
+﻿import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { DiscoveryModule } from "./discovery/discovery.module";
+import { SwipesModule } from "./swipes/swipes.module";
+import { MatchesModule } from "./matches/matches.module";
+import { ConversationsModule } from "./conversations/conversations.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, DiscoveryModule, SwipesModule, MatchesModule, ConversationsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    DiscoveryModule,
+    SwipesModule,
+    MatchesModule,
+    ConversationsModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
