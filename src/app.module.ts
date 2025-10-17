b06f685fd3c03ad58294231c7f6dafcd2cf310a4
EricMota <ericmotinha@yahoo.com.br>
2025-10-17 08:05:28 -0300

    feat(auth,users): scaffolding inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-﻿import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { DbModule } from "./db/db.module";
-import { DbController } from "./db/db.controller";
+﻿// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DiscoveryModule } from './discovery/discovery.module';
+import { SwipesModule } from './swipes/swipes.module';
+import { MatchesModule } from './matches/matches.module';
+import { ConversationsModule } from './conversations/conversations.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule],
-  controllers: [AppController, DbController],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, DiscoveryModule, SwipesModule, MatchesModule, ConversationsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
