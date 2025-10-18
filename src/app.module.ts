92dfe8ceccc1636ea0d090179273c034c31604a5
EricMota <ericmotinha@yahoo.com.br>
2025-10-18 08:15:27 -0300

    fix(app): restaura AppModule e inclui ProfilesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,17 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { DbModule } from "./db.module";
-import { UsersModule } from "./users/users.module";`nimport { ProfilesModule } from "./profiles/profiles.module";
+import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
+import { ProfilesModule } from "./profiles/profiles.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DbModule,
-    UsersModule, ProfilesModule,
+    UsersModule,
     AuthModule,
+    ProfilesModule,
   ],
   controllers: [AppController],
 })
